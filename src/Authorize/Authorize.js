4a71364ac9c6857af7d401f33198e867abc6908f
nabil-k <duomaster01@gmail.com>
2020-09-08 21:54:15 -0700

    created container for 'Connect Spotify' button

--- src/Authorize/Authorize.js
+++ src/Authorize/Authorize.js
@@ -13,9 +13,13 @@ class Authorize extends React.Component{
             <div id="container">
                 <div id="backgroundImage" style={ {backgroundImage: `url(${backgroundImg}` } }></div>
                 <div id="header">
-                    <h1>Welcome, to get started connect your spotify premium account</h1>
-                    <div id="authorizeButton">
-                        <a href="https://accounts.spotify.com/authorize?client_id=6870c94124564cf595d465acfd572c7a&response_type=code&scope=streaming%20user-read-email%20user-read-private&redirect_uri=http://localhost:3000/home">Connect... ( ~ )</a>
+                    <h1 id="authorizeWelcomeHeader">Welcome! Connect your Spotify premium account to get started.</h1>
+                    <div id="authorizeButtonContainer">
+                        <a id="spotifyAuthLink" href="https://accounts.spotify.com/authorize?client_id=6870c94124564cf595d465acfd572c7a&response_type=code&scope=streaming%20user-read-email%20user-read-private&redirect_uri=http://localhost:3000/home">
+                            <div id="authorizeButton">
+                                Connect Spotify
+                            </div> 
+                        </a>
                     </div>
                 </div>
             </div>
