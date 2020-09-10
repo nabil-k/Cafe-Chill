c5929bfd4256b4723d3fb67269d6a4f8de8fdcd8
nabil-k <duomaster01@gmail.com>
2020-09-09 21:45:14 -0700

    users can send messages w/ ENTER key

--- src/Home/Chat/Input.js
+++ src/Home/Chat/Input.js
@@ -13,34 +13,41 @@ class Input extends React.Component{
             input_message: ""
         };
         
-        this.setInputMessage = this.setInputMessage.bind(this);   
+        this.setInputMessage = this.setInputMessage.bind(this);  
+        this.keyPress = this.keyPress.bind(this); 
     }
 
-        // Sends chat message
-        sendMessage(chatSocket){
-            const message = this.state.input_message;
-            console.log(this.chatSocket.readyState);
-            console.log(message)
-            chatSocket.send(JSON.stringify({
-                'message': message
-            }));
-            // Clears input field after message sent
-            this.setState({
-                input_message: ""
-            })
+    // Sends chat message
+    sendMessage(chatSocket){
+        const message = this.state.input_message;
+        console.log(this.chatSocket.readyState);
+        console.log(message)
+        chatSocket.send(JSON.stringify({
+            'message': message
+        }));
+        // Clears input field after message sent
+        this.setState({
+            input_message: ""
+        })
+    }
+
+    setInputMessage(event){
+        this.setState({
+            input_message: event.target.value
+        })
+    }
+
+    keyPress(event){
+        if(event.keyCode == 13){
+            this.sendMessage(this.chatSocket)
         }
-    
-        setInputMessage(event){
-            this.setState({
-                input_message: event.target.value
-            })
-        }
+    }
 
     render(){
 
         return(
             <div>
-                <input id="chat_input" value={this.state.input_message} onChange={this.setInputMessage} placeholder="Say Something... :)" type="text"/>
+                <input id="chat_input" value={this.state.input_message} onKeyDown={this.keyPress} onChange={this.setInputMessage} placeholder="Say Something... :)" type="text"/>
                 <div>
                     <button onClick={ ()=> this.sendMessage(this.chatSocket) } id="send_button">Send</button>
                 </div>
